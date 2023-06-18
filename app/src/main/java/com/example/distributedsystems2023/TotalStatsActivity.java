d78f5e51972769b6be0742d3f1b11a6dec3545e4
Kharnifex <61503559+Kharnifex@users.noreply.github.com>
2023-06-18 15:06:28 +0300

    Back buttons + finishing xml stuff for activities

--- app/src/main/java/com/example/distributedsystems2023/TotalStatsActivity.java
+++ app/src/main/java/com/example/distributedsystems2023/TotalStatsActivity.java
@@ -1,7 +1,7 @@
 package com.example.distributedsystems2023;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,10 +22,19 @@ public class TotalStatsActivity extends AppCompatActivity {
     }
 
     private void loadValues() {
+        //TODO: create socket connection and fetch the 4 float values from the server, then put them as arguments for the 4 setText() methods and uncomment them
+        //binding.DistValue.setText();
+        //binding.ElevationValue.setText();
+        //binding.TimeValue.setText();
+        //binding.SpeedValue.setText();
     }
 
     private void assignBackButtonListener() {
-
+        binding.BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
-
 }
--- app/src/main/java/com/example/distributedsystems2023/WalkStatsActivity.java
+++ app/src/main/java/com/example/distributedsystems2023/WalkStatsActivity.java
@@ -1,4 +1,40 @@
 package com.example.distributedsystems2023;
 
-public class WalkStatsActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.distributedsystems2023.databinding.ActivityWalkStatsBinding;
+
+public class WalkStatsActivity extends AppCompatActivity {
+
+    private ActivityWalkStatsBinding binding;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityWalkStatsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        this.assignBackButtonListener();
+        this.loadValues();
+    }
+
+    private void loadValues() {
+        //TODO: create socket connection and fetch the 4 float values from the server, then put them as arguments for the 4 setText() methods and uncomment them
+        //binding.DistValue.setText();
+        //binding.ElevationValue.setText();
+        //binding.TimeValue.setText();
+        //binding.SpeedValue.setText();
+    }
+
+    private void assignBackButtonListener() {
+        binding.BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
 }
